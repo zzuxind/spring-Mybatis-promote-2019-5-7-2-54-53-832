05b3345317f85b0f062cbfbe42da01fa51debc41
dingxin <zzuxind@163.com>
2019-08-08 09:56:35 +0800

    pageHelper实现分页 添加PageHelper Spring Boot Starter 插件

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.UserMapper;
 import com.example.demo.entity.User;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,24 +15,30 @@ public class UserController {
     private UserMapper userMapper;
 
     @GetMapping("/user")
-    public List<User> findAll() {
-        return null;
+    public List<User> findAll(@RequestParam int pageNum,@RequestParam int pageSize) {
+        PageHelper.startPage(pageNum,pageSize);
+        List<User> users=userMapper.queryList();
+        return users;
     }
 
     @GetMapping("/user/{userId}")
     public User findById(@PathVariable long userId) {
-        return null;
+        User user=userMapper.getUserById(userId);
+        return user;
     }
 
     @PostMapping("/user")
     public void insertUser(@RequestBody User user) {
+        userMapper.insertNewUser(user);
     }
 
     @PutMapping("/user/{userId}")
-    public void updateUser(@RequestBody User user,@PathVariable int userId) {
+    public void updateUser(@PathVariable long userId,@RequestBody User user) {
+        userMapper.updataUser(userId,user);
     }
 
     @DeleteMapping("/user/{userId}")
     public void deleteById(@PathVariable long userId) {
+        userMapper.delete(userId);
     }
 }
--- src/main/java/com/example/demo/dao/UserMapper.java
+++ src/main/java/com/example/demo/dao/UserMapper.java
@@ -1,17 +1,39 @@
 package com.example.demo.dao;
 
+import com.example.demo.entity.User;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserMapper {
     // 1. 实现查询所有用户
+    @Select("select * from user")
+    @Results({
+            @Result(property = "id",column = "id"),
+            @Result(property = "name",column = "name")
+    })
+    public List<User> queryList();
 
     // 2. 根据用户id查询用户
+    @Select("select * from user where id=#{id}")
+    @Results({
+            @Result(property = "id",column = "id"),
+            @Result(property = "name",column = "name")
+    })
+    public User getUserById(long id);
 
     // 3. 添加新用户
+    @Insert( "insert into user(id,name) values (#{id},#{name})")
+    public int insertNewUser(User user);
 
     // 4. 根据用户id删除用户
+    @Delete("delete from user where id=#{id}")
+    public int delete(long id);
 
     // 5. 根据用户id更新用户
+    @Update("update user set id=#{user.id},name=#{user.name} where id=#{id}")
+    public int updataUser(@Param("id") long id, User user);
 
 }
